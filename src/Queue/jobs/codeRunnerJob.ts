8d7215657b68028bdda5096cee9eed4fdecb9d8b
Juned Ahmed Choudhary <jjahmedchoudhary@gmail.com>
2026-03-23 14:23:59 +0530

    pass error to api from bullmq processor to job status failed reason

--- src/Queue/jobs/codeRunnerJob.ts
+++ src/Queue/jobs/codeRunnerJob.ts
@@ -5,8 +5,13 @@ const codeRunnerJob: Processor = async (job) => {
   console.log(`\n[Worker] Picked up Job ID: ${job.id}`);
   const { code, language, stdin = "" } = job.data;
 
-  const output = await runProgrammingLanguagesCode(job.id!, language, code, stdin);
-  return { output };
+  try {
+    const output = await runProgrammingLanguagesCode(job.id!, language, code, stdin);
+    return { output };
+  } catch (error) {
+    if (error instanceof Error) throw error;
+    throw new Error(String(error));
+  }
 };
 
 export default codeRunnerJob;
