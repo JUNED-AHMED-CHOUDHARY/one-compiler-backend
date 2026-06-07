b6a9eda7100381ac5d33a182737483078885e0a9
Juned Ahmed Choudhary <jjahmedchoudhary@gmail.com>
2026-06-07 19:53:55 +0530

    user role check middleware added validation check

--- src/routes/problemRoutes.ts
+++ src/routes/problemRoutes.ts
@@ -1,11 +1,17 @@
+import { UserRole } from "@prisma/client";
 import { Router } from "express";
 
 import { createDraftProblemController } from "../controllers/problemController";
 import asyncHandler from "../middlewares/asyncHandlerMiddleware";
-import { isUserAuthenticatedMiddleware } from "../middlewares/authMiddleware";
+import { isUserAuthenticatedMiddleware, validateUserRoleMiddleware } from "../middlewares/authMiddleware";
 
 const problemRoutes = Router();
 
-problemRoutes.post("/create-draft-problem", asyncHandler(isUserAuthenticatedMiddleware), asyncHandler(createDraftProblemController));
+problemRoutes.post(
+  "/create-draft-problem",
+  asyncHandler(isUserAuthenticatedMiddleware),
+  validateUserRoleMiddleware([UserRole.ADMIN, UserRole.PROBLEM_SETTER]),
+  asyncHandler(createDraftProblemController)
+);
 
 export default problemRoutes;
