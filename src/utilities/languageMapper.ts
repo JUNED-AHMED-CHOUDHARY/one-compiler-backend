3df7504ffc9106ed2a52126a953708682a493445
Juned Ahmed Choudhary <jjahmedchoudhary@gmail.com>
2026-06-25 21:28:01 +0530

    create mapping helper fns

---
+++ src/utilities/languageMapper.ts
@@ -0,0 +1,16 @@
+import { ProgrammingLanguage } from "@prisma/client";
+
+import { SUPPORTED_PROGRAMMING_LANGUAGES } from "../services/dockerRunner";
+
+type InstantiableError = new (message?: string) => Error;
+const PRISMA_TO_RUNNER_LANGUAGE_MAP: Record<ProgrammingLanguage, SUPPORTED_PROGRAMMING_LANGUAGES> = {
+  [ProgrammingLanguage.CPP]: "cpp",
+  [ProgrammingLanguage.JAVASCRIPT]: "javascript",
+  [ProgrammingLanguage.PYTHON]: "python"
+};
+
+export const getRunnerLanguageFromDBLanguage = (language: ProgrammingLanguage, ErrorClass: InstantiableError = Error): SUPPORTED_PROGRAMMING_LANGUAGES => {
+  const mapped = PRISMA_TO_RUNNER_LANGUAGE_MAP[language];
+  if (!mapped) throw new ErrorClass(`Unsupported programming language for execution: ${language}`);
+  return mapped;
+};
