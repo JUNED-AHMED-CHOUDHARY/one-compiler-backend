440e10bb192330d06131dd4932f11e0158ef97f2
Juned Ahmed Choudhary <jjahmedchoudhary@gmail.com>
2026-08-06 23:36:23 +0530

    Poolsize from env for developement its 1 and for production it should be something else

--- src/config/ENV.ts
+++ src/config/ENV.ts
@@ -13,7 +13,12 @@ const ENV = {
   DATABASE_URL: process.env.DATABASE_URL as string,
   MONGODB_URI: process.env.MONGODB_URI as string,
   JWT_SECRET: process.env.JWT_SECRET as string,
-  SHUTDOWN_TIMEOUT_IN_MS: Number(process.env.SHUTDOWN_TIMEOUT_IN_MS) || 7000
+  SHUTDOWN_TIMEOUT_IN_MS: Number(process.env.SHUTDOWN_TIMEOUT_IN_MS) || 7000,
+  POOL_SIZE: {
+    JS_POOL_SIZE: process.env.JS_POOL_SIZE || 1,
+    PYTHON_POOL_SIZE: process.env.PYTHON_POOL_SIZE || 1,
+    CPP_POOL_SIZE: process.env.CPP_POOL_SIZE || 1
+  }
 } as const;
 
 export default ENV;
