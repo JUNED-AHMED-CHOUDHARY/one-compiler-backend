2bfc07fbdbd85d36a5581897e7b44df2365e0ba4
Juned Ahmed Choudhary <jjahmedchoudhary@gmail.com>
2026-08-08 22:39:12 +0530

    increasing payload size because code can be too big

--- src/index.ts
+++ src/index.ts
@@ -23,8 +23,8 @@ const port = Number(ENV.PORT) || 7000;
 app.use(cors());
 app.use(requestLoggerMiddleware);
 app.use(helmet());
-app.use(express.json({ limit: "1mb" }));
-app.use(express.urlencoded({ extended: true, limit: "1mb" }));
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 
 // routes...
 app.use(indexRoutes);
